2016ee2afc0cee7eb7255b09056f10a4834e90c6
PiotrZinka <103209293+PiotrZinka@users.noreply.github.com>
2022-05-10 20:52:51 +0200

    Update custom-additional.js

--- js/custom-additional.js
+++ js/custom-additional.js
@@ -7,9 +7,9 @@ $(document).ready(function(){
       theme: 'bootstrap-5',
       selectionCssClass: 'cs-h40-style',
   } );
-  $( '.cs-h40s' ).select2( {
+  $( '.cs-h40n' ).select2( {
       theme: 'bootstrap-5',
-      selectionCssClass: 'cs-h40s-style',
+      selectionCssClass: 'cs-h40n-style',
       width: '100%'
   } );
 });
